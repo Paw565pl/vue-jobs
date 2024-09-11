b366f056da67913a66d0efd1b3407728ee597d2a
Paw565pl <paw565pl@gmail.com>
2024-09-11 18:41:34 +0200

    feat: add components to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import HomeComponent from "../components/HomeComponent.vue"
+import BaseHero from "@/components/base/BaseHero.vue"
+import HomeCards from "@/components/HomeCards.vue"
+import JobListings from "@/components/JobListings.vue"
 </script>
 
 <template>
   <main>
-    <HomeComponent />
+    <BaseHero title="Become a Vue Developer" subtitle="Find the Vue job that fits your skills and needs" />
+    <HomeCards />
+    <JobListings :limit="3" :show-all-jobs-button="true" />
   </main>
 </template>
