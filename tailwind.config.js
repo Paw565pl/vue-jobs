8fb6c649041c6c651894a9c70eb008e379ed10c2
Paw565pl <paw565pl@gmail.com>
2024-09-11 17:10:32 +0200

    chore: set font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ import daisyui from "daisyui"
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"]
+      }
+    }
   },
   plugins: [daisyui]
 }
