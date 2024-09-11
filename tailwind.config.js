7349ddf6365da0407639ce7972de9880a27c7d65
Paw565pl <paw565pl@gmail.com>
2024-09-11 20:08:55 +0200

    chore: remove daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-import daisyui from "daisyui"
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
@@ -10,5 +8,5 @@ export default {
       }
     }
   },
-  plugins: [daisyui]
+  plugins: []
 }
