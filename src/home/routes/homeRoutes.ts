250da4020669d033454f8f156a2a88e1cd3224b5
Paw565pl <paw565pl@gmail.com>
2024-09-12 14:17:49 +0200

    chore: use lazy import

--- src/home/routes/homeRoutes.ts
+++ src/home/routes/homeRoutes.ts
@@ -1,11 +1,10 @@
-import HomeView from "@/home/views/HomeView.vue"
 import type { RouteRecordRaw } from "vue-router"
 
 const homeRoutes: readonly RouteRecordRaw[] = [
   {
     path: "/",
     name: "home",
-    component: HomeView
+    component: () => import("@/home/views/HomeView.vue")
   }
 ]
 
