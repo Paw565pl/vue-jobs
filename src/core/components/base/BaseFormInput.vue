31dae5bb0437a62b1b5212ce342352786b1c19a4
Paw565pl <paw565pl@gmail.com>
2024-09-13 17:04:16 +0200

    fix: assigning rest of props

--- src/core/components/base/BaseFormInput.vue
+++ src/core/components/base/BaseFormInput.vue
@@ -5,17 +5,16 @@ import type { InputHTMLAttributes } from "vue"
 interface BaseFormInputProps extends /* @vue-ignore */ InputHTMLAttributes {
   label: string
   name: string
+  id: string
 }
 
-const { label, name, ...props } = defineProps<BaseFormInputProps>()
+const { label, name, id } = defineProps<BaseFormInputProps>()
 
-const { value, errorMessage } = useField(() => name)
+const { value, errorMessage } = useField<string>(() => name)
 </script>
 
 <template>
-  <div>
-    <label :for="props.id" class="mb-2 block font-bold text-gray-700">{{ label }}</label>
-    <input class="mb-2 w-full rounded border px-3 py-2" v-bind="props" v-model="value" :name="name" />
-    <p class="text-red-500 text-sm" as="p">{{ errorMessage }}</p>
-  </div>
+  <label :for="id" class="mb-2 block font-bold text-gray-700">{{ label }}</label>
+  <input class="mb-2 w-full rounded border px-3 py-2" v-bind="$attrs" v-model="value" :name="name" :id="id" />
+  <p class="text-red-500 text-sm" as="p">{{ errorMessage }}</p>
 </template>
--- src/core/components/base/BaseFormSelect.vue
+++ src/core/components/base/BaseFormSelect.vue
@@ -5,23 +5,22 @@ import type { SelectHTMLAttributes } from "vue"
 interface BaseFormSelectProps extends /* @vue-ignore */ SelectHTMLAttributes {
   label: string
   name: string
+  id: string
   options: {
     label: string
     value: string
   }[]
 }
 
-const { label, name, options, ...props } = defineProps<BaseFormSelectProps>()
+const { label, name, id, options } = defineProps<BaseFormSelectProps>()
 
-const { value, errorMessage } = useField(() => name)
+const { value, errorMessage } = useField<string>(() => name)
 </script>
 
 <template>
-  <div>
-    <label :for="props.id" class="mb-2 block font-bold text-gray-700">{{ label }}</label>
-    <select class="mb-2 w-full rounded border px-3 py-2" v-bind="props" v-model="value" :name="name">
-      <option v-for="option in options" :key="option.value" :value="option.value">{{ option.label }}</option>
-    </select>
-    <p class="text-red-500 text-sm" as="p">{{ errorMessage }}</p>
-  </div>
+  <label :for="id" class="mb-2 block font-bold text-gray-700">{{ label }}</label>
+  <select class="mb-2 w-full rounded border px-3 py-2" v-bind="$attrs" v-model="value" :name="name" :id="id">
+    <option v-for="option in options" :key="option.value" :value="option.value">{{ option.label }}</option>
+  </select>
+  <p class="text-red-500 text-sm" as="p">{{ errorMessage }}</p>
 </template>
