e90374a545ac7448be0406a804fe76f6d4ee1471
Paw565pl <paw565pl@gmail.com>
2024-09-13 18:59:30 +0200

    refactor: move ShowAllJobsButton to home package

--- src/home/views/HomeView.vue
+++ src/home/views/HomeView.vue
@@ -2,12 +2,14 @@
 import BaseHero from "@/core/components/base/BaseHero.vue"
 import HomeCards from "@/home/components/HomeCards.vue"
 import JobOffersGrid from "@/jobOffer/components/JobOffersGrid.vue"
+import ShowAllJobsButton from "@/home/components/ShowAllJobsButton.vue"
 </script>
 
 <template>
   <main>
     <BaseHero title="Become a Vue Developer" subtitle="Find the Vue job that fits your skills and needs" />
     <HomeCards />
-    <JobOffersGrid :limit="3" :displayShowAllJobsButton="true" />
+    <JobOffersGrid :limit="3" />
+    <ShowAllJobsButton />
   </main>
 </template>
--- src/jobOffer/components/JobOffersGrid.vue
+++ src/jobOffer/components/JobOffersGrid.vue
@@ -1,15 +1,13 @@
 <script lang="ts" setup>
 import BaseLoadingIndicator from "@/core/components/base/BaseLoadingIndicator.vue"
 import JobOfferCard from "@/jobOffer/components/JobOfferCard.vue"
-import ShowAllJobsButton from "@/jobOffer/components/ShowAllJobsButton.vue"
 import useFetchJobOffers from "@/jobOffer/composables/useFetchJobOffers"
 
 interface JobOffersGridProps {
   limit?: number
-  displayShowAllJobsButton?: boolean
 }
 
-const { limit, displayShowAllJobsButton = false } = defineProps<JobOffersGridProps>()
+const { limit } = defineProps<JobOffersGridProps>()
 
 const { data: jobOffers, isLoading } = useFetchJobOffers()
 </script>
@@ -20,16 +18,15 @@ const { data: jobOffers, isLoading } = useFetchJobOffers()
       <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">Browse Jobs</h2>
     </div>
 
-    <BaseLoadingIndicator v-if="isLoading" />
+    <div class="grid grid-cols-1 gap-6 md:grid-cols-3 max-w-7xl mx-auto">
+      <BaseLoadingIndicator v-if="isLoading" />
 
-    <div v-else class="grid grid-cols-1 gap-6 md:grid-cols-3 max-w-7xl mx-auto">
       <JobOfferCard
+        v-else
         v-for="jobOffer in jobOffers?.slice(0, limit ?? jobOffers.length)"
         :key="jobOffer.id"
         :job-offer="jobOffer"
       />
     </div>
   </section>
-
-  <ShowAllJobsButton v-if="displayShowAllJobsButton" />
 </template>
