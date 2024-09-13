6bcc7f607a917f86251d6950934f26261d1d3d6c
Paw565pl <paw565pl@gmail.com>
2024-09-13 17:04:36 +0200

    feat: create BaseFormTextArea component

---
+++ src/core/components/base/BaseFormTextArea.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { useField } from "vee-validate"
+import { type TextareaHTMLAttributes } from "vue"
+
+interface BaseFormTextAreaProps extends /* @vue-ignore */ TextareaHTMLAttributes {
+  label: string
+  name: string
+  id: string
+}
+
+const { label, name, id } = defineProps<BaseFormTextAreaProps>()
+
+const { value, errorMessage } = useField<string>(() => name)
+</script>
+
+<template>
+  <label :for="id" class="mb-2 block font-bold text-gray-700">{{ label }}</label>
+  <textarea class="w-full rounded border px-3 py-2" v-bind="$attrs" v-model="value" :name="name" :id="id"></textarea>
+  <p class="text-red-500 text-sm" as="p">{{ errorMessage }}</p>
+</template>
